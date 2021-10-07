fa1875346d9bd2c9c2f71e39a39636061720f3ca
MaxiChapur <86209975+MaxiChapur@users.noreply.github.com>
2021-10-06 21:21:32 -0300

    Update GetStarted.jsx

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -6,8 +6,7 @@ const GetStarted = () => {
   const data = {
     client_id: '2caa5284ddc648bc983617039538a30c',
     response_type: 'token',
-    //redirect_uri: 'http://localhost:3000/auth', ---> 
-    redirect_uri: 'https://obscure-peak-88695.herokuapp.com/'
+    redirect_uri: 'http://localhost:3000/auth',
     scope: 'user-read-private user-read-email user-library-read',
   }
 
