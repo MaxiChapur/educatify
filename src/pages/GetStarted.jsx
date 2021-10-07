bfe54e303a8b5eaf65810eafea9da6a02fbe2b84
MaxiChapur <86209975+MaxiChapur@users.noreply.github.com>
2021-10-06 22:41:47 -0300

    fixing library

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -16,7 +16,8 @@ const GetStarted = () => {
     <div>
       <h1>Educatify</h1>
       <p>
-        Welcome to Educatify. Use this website to see things in Spotifty <br />
+        Welcome to Educatify.
+        <br /> Use this website to check your favorite tracks, artists and albums from Spotify. <br />
         This web is made for educational purposes.
       </p>
       <div className="button_GetStarted" onClick={() => (window.location.href = url + querystring)}>
