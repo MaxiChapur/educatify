a5a6b788d7cf3b65282734e15a8fb0baf325fee8
maxiChapur <ereshkigal@live.com.ar>
2021-10-07 15:01:48 -0300

    modified redirect

--- src/pages/GetStarted.jsx
+++ src/pages/GetStarted.jsx
@@ -6,7 +6,7 @@ const GetStarted = () => {
   const data = {
     client_id: '2caa5284ddc648bc983617039538a30c',
     response_type: 'token',
-    redirect_uri: 'https://obscure-peak-88695.herokuapp.com/auth',
+    redirect_uri: 'https://educatify.herokuapp.com/auth',
     scope: 'user-read-private user-read-email user-library-read',
   }
 
